0be5ec0faecb6764e6ab93fe563cad6b0639cf4c
InnaLyksa <108470037+InnaLyksa@users.noreply.github.com>
2022-09-09 19:16:45 +0000

    Deploying to gh-pages from @ SergiiBartytskyi/bl-63-parcel@7a9cfa958c7516c6a43f3b6280df6109763e9468 🚀

---
+++ index.437f5bdb.js
@@ -0,0 +1,2 @@
+$(".kickstart-list").slick({infinite:!0,slidesToShow:4,slidesToScroll:3,dots:!0,nextArrow:".our-next-btn",arrows:!0,prevArrow:".our-prev-btn"});
+//# sourceMappingURL=index.437f5bdb.js.map
---
+++ index.c0bc883c.js
@@ -0,0 +1,2 @@
+$(".kickstart-list").slick({infinite:!0,slidesToShow:4,slidesToScroll:3,dots:!0,nextArrow:".our-next-btn",arrows:!0,prevArrow:".our-prev-btn"});
+//# sourceMappingURL=index.c0bc883c.js.map
